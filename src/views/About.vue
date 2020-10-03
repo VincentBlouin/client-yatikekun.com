4a998af1354496ae6b981b4d3b20a2c5cb18318c
Vincent Blouin <vincent.blouin@gmail.com>
2020-10-02 21:53:02 -0400

    new design approach

--- src/views/About.vue
+++ src/views/About.vue
@@ -15,144 +15,189 @@
       </v-row>
     </v-parallax>
     <!--    <Login></Login>-->
-    <v-row align="center" class="vh-center pt-6">
-      <v-col class="text-center" cols="12" md="8">
-        <v-img
-            :src="require('@/assets/banner.png')"
-        >
-        </v-img>
+    <!--    <v-subheader class="text-subtitle-1">-->
+    <!--      <v-spacer></v-spacer>-->
+    <!--      <v-icon class="mr-2">star</v-icon>-->
+    <!--      {{ $t('about:plus1') }}-->
+    <!--      <v-spacer></v-spacer>-->
+    <!--    </v-subheader>-->
+
+    <!--    <v-card-title class="text-center text-h3">-->
+    <!--      <v-spacer></v-spacer>-->
+    <!--      {{ $t('about:general1') }}-->
+    <!--      <v-spacer></v-spacer>-->
+    <!--    </v-card-title>-->
+    <!--    <v-card-subtitle class="text-h6 font-weight-regular">-->
+    <!--      {{ $t('about:generalSubtitle') }}-->
+    <!--    </v-card-subtitle>-->
+    <v-row align="center" class="vh-center">
+      <v-col class="text-center" cols="12">
+        <v-card-text class="text-h6 font-weight-regular vh-center">
+          <v-list>
+            <v-list-item v-if="$vuetify.breakpoint.mdAndUp">
+              <v-list-item-icon>
+                <v-icon large>list</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content class="text-left">
+                <v-list-item-title class="text-md-h6 font-weight-regular">
+                  {{ $t('about:plusOffers') }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item v-if="$vuetify.breakpoint.smAndDown">
+              <v-list-item-icon>
+                <v-icon large>list</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content class="text-left">
+                <v-list-item-title class="text-sm-body-1 font-weight-regular">
+                  {{ $t('about:plusOffersMobile') }}
+                </v-list-item-title>
+                <v-list-item-subtitle>
+                  {{ $t('about:plusOffersSubtitleMobile') }}
+                </v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon large>hourglass_bottom</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content class="text-left">
+                <v-list-item-title class="text-md-h6 text-sm-body-1 font-weight-regular">
+                  {{ $t('about:plusTransactions') }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-icon>
+                <v-badge overlap>
+                  <template slot="badge">
+                    5
+                  </template>
+                  <v-icon large>money</v-icon>
+                </v-badge>
+              </v-list-item-icon>
+              <v-list-item-content class="text-left">
+                <v-list-item-title class="text-md-h6 text-sm-body-1 font-weight-regular">
+                  {{ $t('about:plus5Hours') }}
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item v-if="$vuetify.breakpoint.mdAndUp">
+              <v-list-item-icon>
+                <v-icon large>emoji_people</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content class="text-left">
+                <v-list-item-title class="text-md-h6 text-sm-body-1 font-weight-regular">
+                  {{ $t('about:plusRequests') }}
+                  <a href="https://www.facebook.com/seldemer.gratis">
+                    <v-badge color="transparent" inline>
+                      <template v-slot:badge>
+                        <img :src="require('@/assets/f_logo_RGB-Blue_58.png')" height="20">
+                      </template>
+                      {{ $t('about:plusRequestsFacebook') }}
+                    </v-badge>
+                  </a>
+                </v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+          <!--            <p>-->
+          <!--              {{ $t('about:general3') }}-->
+          <!--              <a href="https://www.facebook.com/seldemer.gratis">-->
+          <!--                <v-badge color="transparent">-->
+          <!--                  <template v-slot:badge>-->
+          <!--                    <img :src="require('@/assets/f_logo_RGB-Blue_58.png')" height="20">-->
+          <!--                  </template>-->
+          <!--                  {{ $t('about:general3A') }}-->
+          <!--                </v-badge>-->
+          <!--              </a>-->
+          <!--            </p>-->
+          <!--            <p>-->
+          <!--              {{ $t('about:general4') }}-->
+          <!--              <a href="https://loco-local.net">{{ $t('about:general5') }}</a>,-->
+          <!--              {{ $t('about:general6') }}-->
+          <!--            </p>-->
+        </v-card-text>
+        <v-card class="pb-0" flat>
+          <v-card-actions class="vh-center">
+            <v-btn
+                color="primary"
+                x-large
+                class="pa-8 font-weight-bold pl-16 pr-16"
+            >
+              <v-icon class="mr-4 how_to_reg" x-large></v-icon>
+              {{$t('about:becomeMember')}}
+            </v-btn>
+          </v-card-actions>
+          <v-row align="center" class="vh-center">
+            <v-col class="text-center" cols="12" md="8">
+              <v-img
+                  :src="require('@/assets/banner.png')"
+              >
+              </v-img>
+            </v-col>
+          </v-row>
+        </v-card>
       </v-col>
     </v-row>
     <v-row align="center" class="vh-center">
-      <v-col class="text-center" cols="12" md="6">
-        <v-card flat>
-          <v-card-title class="text-center text-h5">
-            <v-spacer></v-spacer>
-            {{ $t('about:general1') }}
-            <a class="pl-2" @click="whatIsTimeBank = true;">
-              <v-badge
-                  icon="mdi-help"
-              >
-                {{ $t('about:general2') }}
-              </v-badge>
-            </a>
-            <v-spacer></v-spacer>
-          </v-card-title>
-          <v-card-text class="text-subtitle-1">
-            <p>
-              {{ $t('about:general3') }}
-              <a href="https://www.facebook.com/seldemer.gratis">
-                <v-badge color="transparent">
-                  <template v-slot:badge>
-                    <img :src="require('@/assets/f_logo_RGB-Blue_58.png')" height="20">
-                  </template>
-                  {{ $t('about:general3A') }}
-                </v-badge>
-              </a>
-            </p>
-            <p>
-              {{ $t('about:general4') }}
-              <a href="https://loco-local.net">{{ $t('about:general5') }}</a>,
-              {{ $t('about:general6') }}
-            </p>
-          </v-card-text>
-        </v-card>
+      <v-col class="text-center" cols="6">
         <v-card>
-          <v-tabs fixed-tabs v-model="aboutMembershipTabs"    icons-and-text>
-            <v-tab>
-              {{ $t('about:member1') }}
-              <v-icon>how_to_reg</v-icon>
-            </v-tab>
-            <v-tab>
-              {{ $t('about:plus1') }}
-              <v-icon>star</v-icon>
-            </v-tab>
-          </v-tabs>
-          <v-tabs-items v-model="aboutMembershipTabs">
-            <v-tab-item>
-              <v-list>
-                <v-list-item>
-                  <v-list-item-icon>
-                    <v-badge>
-                      <template v-slot:badge>
-                        1
-                      </template>
-                      <v-icon>supervisor_account</v-icon>
-                    </v-badge>
-                  </v-list-item-icon>
-                  <v-list-item-content class="text-left">
-                    <v-list-item-title>
-                      {{ $t('about:member2') }}
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-                <v-list-item>
-                  <v-list-item-icon>
-                    <v-badge>
-                      <template v-slot:badge>
-                        2
-                      </template>
-                      <v-icon>assignment</v-icon>
-                    </v-badge>
-                  </v-list-item-icon>
-                  <v-list-item-content class="text-left">
-                    <v-list-item-title>
-                      {{ $t('about:member3') }}
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-                <v-list-item>
-                  <v-list-item-icon>
-                    <v-badge>
-                      <template v-slot:badge>
-                        3
-                      </template>
-                      <v-icon>card_membership</v-icon>
-                    </v-badge>
-                  </v-list-item-icon>
-                  <v-list-item-content class="text-left">
-                    <v-list-item-title>
-                      {{ $t('about:member4') }}
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-list>
-            </v-tab-item>
-            <v-tab-item>
-              <v-list>
-                <v-list-item>
-                  <v-list-item-icon>
-                    <v-icon>people</v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-content class="text-left">
-                    <v-list-item-title>
-                      {{ $t('about:member2') }}
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-                <v-list-item>
-                  <v-list-item-icon>
+          <v-card-text class="vh-center">
+            <v-list subheader>
+              <v-subheader class="text-md-h5 text-sm-subtitle-1 font-weight-regular mb-7">
+                <v-spacer></v-spacer>
+                <v-icon x-large>how_to_reg</v-icon>
+                {{ $t('about:member1') }}
+                <v-spacer></v-spacer>
+              </v-subheader>
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-badge>
+                    <template v-slot:badge>
+                      1
+                    </template>
+                    <v-icon>supervisor_account</v-icon>
+                  </v-badge>
+                </v-list-item-icon>
+                <v-list-item-content class="text-left">
+                  <v-list-item-title class="text-md-h6 text-sm-body-1 font-weight-regular">
+                    {{ $t('about:member2') }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-badge>
+                    <template v-slot:badge>
+                      2
+                    </template>
                     <v-icon>assignment</v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-content class="text-left">
-                    <v-list-item-title>
-                      {{ $t('about:member3') }}
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-                <v-list-item>
-                  <v-list-item-icon>
+                  </v-badge>
+                </v-list-item-icon>
+                <v-list-item-content class="text-left">
+                  <v-list-item-title class="text-md-h6 text-sm-body-1 font-weight-regular">
+                    {{ $t('about:member3') }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+              <v-list-item>
+                <v-list-item-icon>
+                  <v-badge>
+                    <template v-slot:badge>
+                      3
+                    </template>
                     <v-icon>card_membership</v-icon>
-                  </v-list-item-icon>
-                  <v-list-item-content class="text-left">
-                    <v-list-item-title>
-                      {{ $t('about:member4') }}
-                    </v-list-item-title>
-                  </v-list-item-content>
-                </v-list-item>
-              </v-list>
-            </v-tab-item>
-          </v-tabs-items>
+                  </v-badge>
+                </v-list-item-icon>
+                <v-list-item-content class="text-left">
+                  <v-list-item-title class="text-md-h6 text-sm-body-1 font-weight-regular">
+                    {{ $t('about:member4') }}
+                  </v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-card-text>
         </v-card>
       </v-col>
     </v-row>
@@ -182,18 +227,25 @@ export default {
       vision: "S'entraider",
       vision2: "Avec une liste d'offres et demandes de services dans la Baie des Chaleurs",
       vision3: "Avec une monnaie locale pour offrir et reçevoir des services avec le principe 1 heure vaut 1 heure",
-      general1: "Le SEL de mer est un",
+      becomeMember: "Devenez membre !",
+      general1: "SEL de Mer",
+      generalSubtitle: "Échanges de services avec le principe une heure vaut une heure",
       general2: "système d'échange de services",
-      general3: "Ce site permet d'afficher des offres de services ainsi que d'enregistrer des transactions d'heures. Les demandes se réalisent sur le",
-      general3A: "groupe facebook privé",
-      general4: "C'est un projet porté par un groupe de citoyennes et de citoyens et",
-      general5: "Horizons Gaspésiens",
-      general6: "avec l'objectif de réduire la pauvreté et d'augmenter l'entraide.",
-      member1: "Comment devenir membre",
+      // general3: "Ce site permet d'afficher des offres de services ainsi que d'enregistrer des transactions d'heures. Les demandes se réalisent sur le",
+      plusOffers: "Consultez les offres de la Baie des Chaleurs et ajouter la votre",
+      plusOffersMobile: "Consultez les offres de la BDC",
+      plusOffersSubtitleMobile: "Et ajouter la votre",
+      plusTransactions: "Enregistrez des transactions d'heures",
+      plus5Hours: "Obtenez 5 heures de service à l'enregistrement",
+      plusRequests: "Consultez les demandes de services sur le",
+      plusRequestsFacebook: "groupe facebook privé",
+      // general4: "C'est un projet porté par un groupe de citoyennes et de citoyens et",
+      // general6: "avec l'objectif de réduire la pauvreté et d'augmenter l'entraide.",
+      member1: "Pour devenir membre",
       member2: "Prendre rendez-vous avec un administrateur",
       member3: "Lire et accepter la charte",
       member4: "Devenir membre de la coopérative de solidarité Horizons Gaspésiens",
-      plus1: "Avantage d'être membre"
+      plus1: "Le SEL de mer vous permet de"
     });
     I18n.i18next.addResources("en", "about", {
       vision: "S'entraider",
