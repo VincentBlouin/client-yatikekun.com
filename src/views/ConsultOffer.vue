98df4998f83fb857767cfc16fce941215b8daca6
Vincent Blouin <vincent.blouin@gmail.com>
2020-12-03 10:08:51 -0500

    design consult

--- src/views/ConsultOffer.vue
+++ src/views/ConsultOffer.vue
@@ -5,21 +5,43 @@
           color="primary"
           large
           class="white--text subtitle-1 pl-4 pr-4"
+          :class="{
+            'pb-7 pt-7': $vuetify.breakpoint.smAndDown
+          }"
       >
-        <v-icon class="mr-2">phone</v-icon>
-        {{ $t('consult:contact') }}
+        <v-row v-if="$vuetify.breakpoint.smAndDown">
+          <v-col cols="12" class="pa-0 pb-0">
+            <v-icon>person</v-icon>
+          </v-col>
+          <v-col cols="12" class="pa-0 pt-1">
+            {{ $t('consult:contact') }}
+          </v-col>
+        </v-row>
+        <v-icon class="mr-2" v-if="$vuetify.breakpoint.mdAndUp">person</v-icon>
+        <span v-if="$vuetify.breakpoint.mdAndUp">{{ $t('consult:contact') }}</span>
       </v-btn>
       <v-divider vertical class="ml-4 mr-4"></v-divider>
       <v-btn
           color="primary"
           large
           class="white--text subtitle-1 pl-4 pr-4"
+          :class="{
+            'pb-7 pt-7': $vuetify.breakpoint.smAndDown
+          }"
       >
-        <v-icon class="mr-2">attach_money</v-icon>
-        {{ $t('consult:transaction') }}
+        <v-row v-if="$vuetify.breakpoint.smAndDown">
+          <v-col cols="12" class="pa-0 pb-0">
+            <v-icon>attach_money</v-icon>
+          </v-col>
+          <v-col cols="12" class="pa-0 pt-1">
+            {{ $t('consult:transaction') }}
+          </v-col>
+        </v-row>
+        <v-icon class="mr-2" v-if="$vuetify.breakpoint.mdAndUp">attach_money</v-icon>
+        <span v-if="$vuetify.breakpoint.mdAndUp">{{ $t('consult:transaction') }}</span>
       </v-btn>
     </v-card-actions>
-    <v-card-text class="pt-8">
+    <v-card-text class="">
       <v-row
           align="center"
           justify="center"
@@ -31,18 +53,26 @@
               type="image, list-item-two-line"
           ></v-skeleton-loader>
         </v-col>
-        <v-col cols="12" class="col-md-6 col-lg-3 h-right" v-if="!isLoading">
-          <v-card flat class="text-center h-right mr-8" max-width="300">
+        <v-col cols="12" class="col-md-6 col-lg-3" v-if="!isLoading" :class="{
+          'h-right offer-margin-top': $vuetify.breakpoint.mdAndUp,
+          'vh-center' : $vuetify.breakpoint.smAndDown
+        }">
+          <v-card flat class="text-center pt-0" max-width="300" :class="{
+            'h-right mr-8' : $vuetify.breakpoint.mdAndUp,
+            'vh-center' : $vuetify.breakpoint.smAndDown
+          }">
             <v-img
                 height="200"
                 v-if="offer.image"
                 :src="getCustomImageUrl(offer.image)"
                 :alt="offer.image.name"
+                class="pt-0"
             ></v-img>
             <v-img
                 height="200"
                 v-if="offer.customImage"
                 :src="getCustomImageUrl(offer.customImage)"
+                class="pt-0"
             ></v-img>
             <v-card-text class="subtitle-1 text-right pl-0 pr-0 pb-0 pt-0 font-italic">
               {{ $t(offer.User.subRegion) }}
@@ -53,20 +83,20 @@
           </v-card>
         </v-col>
         <v-col cols="12" class="col-md-6 col-lg-3 text-center">
-          <v-card flat class="pt-0">
-            <v-card-title class="pt-0">
-              {{$t('consult:fees')}}
+          <v-card flat class="">
+            <v-card-title class="">
+              {{ $t('consult:fees') }}
             </v-card-title>
-            <v-card-text class="text-left">
-              {{offer.additionalFees_fr}}
+            <v-card-text class="text-left subtitle-1">
+              {{ offer.additionalFees_fr }}
             </v-card-text>
           </v-card>
           <v-card flat>
             <v-card-title>
-              {{$t('consult:experience')}}
+              {{ $t('consult:experience') }}
             </v-card-title>
-            <v-card-text class="text-left">
-              {{offer.experience_fr}}
+            <v-card-text class="text-left subtitle-1">
+              {{ offer.experience_fr }}
             </v-card-text>
           </v-card>
         </v-col>
@@ -92,14 +122,14 @@ export default {
   },
   data: function () {
     I18n.i18next.addResources("fr", "consult", {
-      contact: "Contacter le membre",
-      transaction: "Inscrire une transaction",
+      contact: "Contacter",
+      transaction: "Transaction",
       experience: "Expérience",
       fees: "Frais additionels"
     });
     I18n.i18next.addResources("en", "offer", {
-      contact: "Contacter le membre",
-      transaction: "Inscrire une transaction",
+      contact: "Contacter",
+      transaction: "Transaction",
       experience: "Expérience",
       fees: "Frais additionels"
     });
@@ -131,4 +161,7 @@ export default {
 }
 </script>
 <style>
+.offer-margin-top {
+  margin-top: -40px;
+}
 </style>
